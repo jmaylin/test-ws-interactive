6c9c116287b224b1816eca23b303abb14a1c8ec7
jmaylin <jordi.maylin@gmail.com>
2017-01-21 23:28:01 +0100

    Set viewport width and height. Randomly position a circle

--- scripts/components/Circle.js
+++ scripts/components/Circle.js
@@ -1,14 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
-import styles from './Circle.css';
+import styles from './Circle.scss';
 
 class Circle extends Component {
   render() {
-    return (
-      <div>
-        <h2>Circle</h2>
-      </div>);
+    const { radius, coordinates } = this.props;
+    return <circle cx={coordinates.x} cy={coordinates.y} r={radius} fill="red"/>;
   }
 }
+
+Component.defaultProps = {
+  radius: 10,
+  coordinates: {
+    x: 0,
+    y: 0
+  }
+};
+
+Component.propTypes = {
+  radius: PropTypes.number,
+  coordinates: PropTypes.shape({
+    x: PropTypes.number.isRequired,
+    y: PropTypes.number.isRequired
+  })
+};
 
 export default Circle;
--- scripts/components/Viewport.js
+++ scripts/components/Viewport.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
+import { random } from 'lodash';
 
 import Circle from './Circle';
 
-import styles from './Viewport.css';
+import './Viewport.scss';
 
 class Viewport extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: 0,
+      height: 0
+    };
+  }
+  getRandomCoordinates(maxX, maxY) {
+    return {
+      x: random(this.state.width),
+      y: random(this.state.height)
+    };
+  }
+  componentWillMount() {
+    this.setState({
+      width: window.innerWidth,
+      height: window.innerHeight
+    })
+  }
   render() {
     return (
-      <div>
-        <h2>Viewport</h2>
-        <Circle></Circle>
-      </div>);
+      <svg className="viewportWrapper" viewBox={`0 0 ${this.state.width} ${this.state.height}`}>
+        <Circle coordinates={this.getRandomCoordinates()} />
+      </svg>
+    );
   }
 }
 
