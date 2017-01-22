a93e5c6657d3762f96841802882cf1216eae9aeb
jmaylin <jordi.maylin@gmail.com>
2017-01-22 19:54:50 +0100

    Add a black circle that represent where the user clicked

--- scripts/components/Viewport.js
+++ scripts/components/Viewport.js
@@ -13,21 +13,31 @@ class Viewport extends Component {
     super(props);
     this.state = {
       width: 0,
-      height: 0
+      height: 0,
+      gravityCenter: {
+        x: 0,
+        y: 0
+      }
     };
+    this.circlesAlreadyGenerated = false;
+    this.setGravityCenter = this.setGravityCenter.bind(this);
   }
 
   generateCircles(numberOfCircles) {
-    const circles = [];
+    if (this.circlesAlreadyGenerated) {
+      return this.circles;
+    }
+    this.circlesAlreadyGenerated = true;
+    this.circles = [];
     for (var i = 0; i < numberOfCircles; i++) {
-      circles.push(<Circle
+      this.circles.push(<Circle
         hue={i/2}
         key={i}
         coordinates={this.getRandomCoordinates()}
       />);
       // TODO generate ghost circle (using viewport center)
     }
-    return circles;
+    return this.circles;
   }
 
   getRandomCoordinates(maxX, maxY) {
@@ -39,23 +49,35 @@ class Viewport extends Component {
 
   setGravityCenter(e) {
     AppActions.setGravityCenter({
-      x: e.screenX,
-      y: e.screenY
+      x: e.pageX,
+      y: e.pageY
+    });
+    this.setState({
+      gravityCenter: {
+        x: e.pageX,
+        y: e.pageY
+      }
     });
   }
 
   componentWillMount() {
     this.setState({
       width: window.innerWidth,
-      height: window.innerHeight
+      height: window.innerHeight,
+      gravityCenter: {
+        x: window.innerWidth / 2,
+        y: window.innerHeight /2
+      }
     })
     // TODO calculate viewport center
   }
 
   render() {
+    const { gravityCenter } = this.state;
     return (
       <svg className="viewportWrapper" viewBox={`0 0 ${this.state.width} ${this.state.height}`} onClick={this.setGravityCenter}>
-        {this.generateCircles(numberOfCircles)}
+        <circle cx={gravityCenter.x} cy={gravityCenter.y} r="3" fill="black"/>
+        { this.generateCircles(numberOfCircles) }
       </svg>
     );
   }
